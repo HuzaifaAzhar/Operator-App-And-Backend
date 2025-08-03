675c22672c292298388b600b0499e7b0364bac0c
Huzaifa Azhar <huzaifaazhar21@gmail.com>
2025-08-03 12:06:37 +0500

    Fix backend for ios

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const db = mysql.createConnection({
   host: 'rm-t4n55s7c50nax1164uo.mysql.singapore.rds.aliyuncs.com',
